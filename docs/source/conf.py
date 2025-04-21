85c35e7a387bb877cabefdf00f016f56175c6d23
vargaheni05 <vargaheni05@gmail.com>
2025-04-21 19:50:19 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,22 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from pyTSPA import __version__
 
 project = 'pyTSPA_toolbox'
 copyright = '2025, Kardos Benedek, Szögi Marcell, Varga Henrietta'
 author = 'Kardos Benedek, Szögi Marcell, Varga Henrietta'
-release = '0.0.1'
+release = __version__
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx.ext.viewcode','sphinx_toolbox.more_autodoc.autotypeddict','enum_tools.autoenum']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_context = {
+    "display_github": True
+}
+
+html_theme_options = {
+    'logo_only': False,
+    'vcs_pageview_mode': ''
+}
