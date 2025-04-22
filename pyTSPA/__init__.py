d07a16b6af372f0c3ae30162569ad20ff6927173
vargaheni05 <vargaheni05@gmail.com>
2025-04-22 18:01:12 +0200

    verzioemeles 0.0.2

--- pyTSPA/__init__.py
+++ pyTSPA/__init__.py
@@ -3,4 +3,4 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("pyTSPA_toolbox")
 except PackageNotFoundError:
-    __version__ = "0.0.1"
+    __version__ = "0.0.2"
