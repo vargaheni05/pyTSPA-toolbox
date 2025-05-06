234cfd451f98fe8371b9d7235df1e1fc2b79fa4e
vargaheni05 <vargaheni05@gmail.com>
2025-05-06 14:51:26 +0200

    init.py bovites

--- pyTSPA/__init__.py
+++ pyTSPA/__init__.py
@@ -6,3 +6,10 @@ except PackageNotFoundError:
     __version__ = "0.0.2"
 
 from .data import load_match_data, clean_data, data_profiling
+
+__all__ = [
+    "load_match_data",
+    "clean_data",
+    "data_profiling",
+    "__version__",
+]
